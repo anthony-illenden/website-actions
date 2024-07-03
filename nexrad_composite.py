37e1eaa04728eaf08e4ba01807d3108f029a8c09
Tony Illenden <130492486+anthony-illenden@users.noreply.github.com>
2024-07-03 13:22:09 -0400

    Update nexrad_composite.py

--- nexrad_composite.py
+++ nexrad_composite.py
@@ -27,18 +27,18 @@ daytime = now.day
 url = 'https://thredds.ucar.edu/thredds/catalog/nexrad/composite/gini/dhr/1km/{}{}{}/catlog.xml'.format(year,str(month).zfill(2),str(daytime).zfill(2))
 
 for i in range(0,11):
-    best_radar=TDSCatalog(url)
-    radar_ds=best_radar.datasets
-    ncss=radar_ds[i].subset()
+    radar_ds = TDSCatalog(url)
+    radar_ds = radar_ds.datasets
+    ncss = radar_ds[i].subset()
     query = ncss.query()
     query.accept('netcdf4')
     query.variables('Reflectivity')
-    radar_data=ncss.get_data(query)
-    radar_data=open_dataset(NetCDF4DataStore(radar_data))
+    radar_data = ncss.get_data(query)
+    radar_data = open_dataset(NetCDF4DataStore(radar_data))
     radar_data_ds = radar_data.metpy.parse_cf().set_coords(['x', 'y'])
     radar_latlon = radar_data_ds.metpy.assign_latitude_longitude()
     radar_ref = radar_latlon['Reflectivity'][0,:,:]
-    radar_ref = np.ma.masked_where(radar_ref < -25, radar_ref)
+    radar_ref = np.ma.masked_where(radar_ref < 5, radar_ref)
     formatted_datetime = extract_and_format_date_time(radar_ds[i].name)
     fig, ax = plt.subplots(figsize=(12, 10), subplot_kw={'projection': ccrs.PlateCarree()})
     ax.set_extent([-91, -81, 40.5, 47.75])
@@ -46,9 +46,11 @@ for i in range(0,11):
     ax.add_feature(cfeature.STATES.with_scale('50m'))
     ax.add_feature(cfeature.BORDERS.with_scale('50m'))
     ax.add_feature(USCOUNTIES.with_scale('5m'), linewidth=0.25)
+    ax.add_feature(cfeature.LAKES, zorder=1, color='#ecf9fd')
+    ax.add_feature(cfeature.LAND, color='#fbf5e9')
     mesh = plt.pcolormesh(radar_latlon['longitude'], radar_latlon['latitude'], radar_ref, cmap=metpy.plots.ctables.registry.get_colortable('NWSStormClearReflectivity'), vmin=-25, vmax=80)
     cbar = plt.colorbar(mesh, ax=ax, orientation='vertical', label='dBZ', fraction=0.046, pad=0.04, shrink=0.80)
     cbar.set_ticks(np.arange(-25, 81, 10))
     plt.title('NEXRAD Composite Reflectivity {}'.format(formatted_datetime))
     plt.savefig('plots/doppler_radar/nexrad/reflectivity_{}.png'.format(i), dpi=450, bbox_inches='tight')
-    plt.show()
+    #plt.show()
