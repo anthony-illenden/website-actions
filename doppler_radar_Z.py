0156aaf59666a40ce4f6512474eb72e616fe47e6
Tony Illenden <130492486+anthony-illenden@users.noreply.github.com>
2024-07-04 12:10:17 -0400

    Update doppler_radar_Z.py

--- doppler_radar_Z.py
+++ doppler_radar_Z.py
@@ -57,6 +57,8 @@ for ds_name in sorted(catalog.datasets):
     ax.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth=2)
     ax.add_feature(cfeature.STATES.with_scale('50m'))
     ax.add_feature(USCOUNTIES.with_scale('5m'), linewidth=0.25)
+    ax.add_feature(cfeature.LAKES, zorder=1, color='#ecf9fd')
+    ax.add_feature(cfeature.LAND, color='#fbf5e9')
     ax.plot(-83.47, 42.7, 'ro', markersize=5, transform=ccrs.PlateCarree(), zorder=2, color='black')
     plt.title('{} Base Reflectivity at {} UTC'.format(data.Station, formatted_datetime), loc='center')
     img = ax.pcolormesh(x, y, ref_clean2, cmap=metpy.plots.ctables.registry.get_colortable('NWSStormClearReflectivity'), vmin=-35, vmax=80, zorder=0)
